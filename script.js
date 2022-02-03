50bb4a6b45782972e40a4b6c43cfb716e4026300
LizaKhvesko <khveskoliza@tut.by>
2022-02-03 14:03:29 +0300

    add some functions

--- script.js
+++ script.js
@@ -3,22 +3,35 @@ const isNumber = function(num) {
 }
 
 function botRandom(x) {
-    let a = prompt('Угадай число от 1 до 100');  
+    let attempts = 10;
+    let a = prompt(`Угадай число от 1 до 100. Количество попыток: ${attempts}`);  
     function answer() {
+        if(attempts <= 1) {
+            a = alert('Попытки закончились, хотите сыграть еще?');
+            if (a === undefined) {
+                botRandom(Math.floor(Math.random() * 100) + 1);
+            }
+        } else {
         if (a === null) {
             a = alert('Игра окончена!');
         } else if(!isNumber(a)) {
             a = prompt('Введи число!');
             answer();
         } else if(a == x) {
-            a = alert('Поздравляю, ты угадал!');
+            a = alert('Поздравляю, ты угадал! Хочещь сыграть еще?');
+            if (a === undefined) {
+                botRandom(Math.floor(Math.random() * 100) + 1);
+            }
         } else if (a > x) {
-            a = prompt('Загаданное число меньше. Попробуй еще!');
+            attempts--
+            a = prompt(`Загаданное число меньше. Попробуй еще! Количество попыток: ${attempts}`);
             answer();
         } else if(a < x) {
-            a = prompt('Загаданное число больше. Попробуй еще!');
+            attempts--
+            a = prompt(`Загаданное число больше. Попробуй еще! Количество попыток: ${attempts}`);
             answer();
         } 
+        }
     }
     answer();
 }
